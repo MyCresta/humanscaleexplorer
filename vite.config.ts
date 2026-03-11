9924606f43e6e2ae385efd812124c12eac98dc9f
My Cresta <itislink@gmail.com>
2026-03-11 08:56:39 +0100

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "/humanscaleexplorer/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 });
